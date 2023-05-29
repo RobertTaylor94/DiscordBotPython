b610a529b6925e5503e95ce6e3bc1106bb2fb173
Robert Taylor <rjt903@gmail.com>
2023-05-29 23:17:32 +0100

    Added rollDamage button with stats for one player

--- buttons.py
+++ buttons.py
@@ -16,3 +16,16 @@ class attackButton(discord.ui.Button):
         else:
             await interaction.response.send_message(f'Attack button pressed by someone')
 
+class rollDamage(discord.ui.Button):
+    def __init__(self):
+        super().__init__(style=discord.ButtonStyle.secondary, label="Roll Damage")
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user.name == 'murvoth':
+            roll1 = int(random.choice(range(1, 10)))
+            roll2 = int(random.choice(range(1, 4)))
+            total = str(roll1 + roll2 + 5)
+            embed = discord.Embed(color=2, title="Damage by Murvoth", description=f'{roll1} + {roll2} + 5 = {total}', type='rich')
+            await interaction.response.send_message(embed=embed)
+        else:
+            await interaction.response.send_message('Damage be damaging')
