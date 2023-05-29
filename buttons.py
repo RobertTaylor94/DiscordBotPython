fa2383ff51d8c004d564b3198de2e545c1119976
Robert Taylor <rjt903@gmail.com>
2023-05-29 23:17:11 +0100

    Changed attack roll result to an embedded message

--- buttons.py
+++ buttons.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 class attackButton(discord.ui.Button):
     def __init__(self):
@@ -6,6 +7,12 @@ class attackButton(discord.ui.Button):
     
     async def callback(self, interaction: discord.Interaction):
         if interaction.user.name == 'murvoth':
-            await interaction.response.send_message(f'Attack button pressed by {interaction.user.name}')
+            roll = int(random.choice(range(1, 20)))
+            total= str(roll+15)
+            view = discord.ui.View()
+            view.add_item(rollDamage())
+            embed = discord.Embed(color=1, title="Attack by Murvoth", description=f'{roll} + 15 = {total}', type='rich')
+            await interaction.response.send_message(view=view, embed=embed)
         else:
             await interaction.response.send_message(f'Attack button pressed by someone')
+
