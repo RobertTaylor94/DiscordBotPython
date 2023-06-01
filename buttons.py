e5a33704c933eb881ccf2f9faa13a8669a23fa43
Robert Taylor <rjt903@gmail.com>
2023-06-01 21:05:25 +0100

    Fixed ranges of dice rolls to include max number

--- buttons.py
+++ buttons.py
@@ -7,11 +7,11 @@ class attackButton(discord.ui.Button):
     
     async def callback(self, interaction: discord.Interaction):
         if interaction.user.name == 'murvoth':
-            roll = int(random.choice(range(1, 20)))
+            roll = int(random.choice(range(1, 21)))
             total= str(roll+15)
             view = discord.ui.View()
             view.add_item(rollDamage())
-            embed = discord.Embed(color=1, title="Attack by Murvoth", description=f'{roll} + 15 = {total}', type='rich')
+            embed = discord.Embed(color=1, title=f'Attack by {interaction.user.name}', description=f'{roll} + 15 = {total}', type='rich')
             await interaction.response.send_message(view=view, embed=embed)
         else:
             await interaction.response.send_message(f'Attack button pressed by someone')
@@ -22,10 +22,11 @@ class rollDamage(discord.ui.Button):
 
     async def callback(self, interaction: discord.Interaction):
         if interaction.user.name == 'murvoth':
-            roll1 = int(random.choice(range(1, 10)))
-            roll2 = int(random.choice(range(1, 4)))
+            roll1 = int(random.choice(range(1, 11)))
+            roll2 = int(random.choice(range(1, 5)))
             total = str(roll1 + roll2 + 5)
-            embed = discord.Embed(color=2, title="Damage by Murvoth", description=f'{roll1} + {roll2} + 5 = {total}', type='rich')
+            embed = discord.Embed(color=2, title=f'Damage by {interaction.user.name}', description=f'{roll1} + {roll2} + 5 = {total}', type='rich')
             await interaction.response.send_message(embed=embed)
         else:
             await interaction.response.send_message('Damage be damaging')
+
