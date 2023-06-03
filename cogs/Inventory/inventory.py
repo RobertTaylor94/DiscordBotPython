15eb679b2df0e7b8867a3e64b6aa5cf10550093b
Robert Taylor <rjt903@gmail.com>
2023-06-03 18:24:53 +0100

    Feature: Added command to remove inventory items

--- cogs/Inventory/inventory.py
+++ cogs/Inventory/inventory.py
@@ -28,7 +28,7 @@ class inventory(commands.Cog):
                 desc = item['description']
                 em.add_field(name=item_name, value=desc)
         
-        await interaction.response.send_message(embed=em)
+        await interaction.response.send_message(embed=em, ephemeral = True)
             
     @app_commands.command(name='add_inventory', description='Add an item to your inventory')
     @app_commands.describe(name='name', description='description')
@@ -50,7 +50,35 @@ class inventory(commands.Cog):
         em = Embed(title=f'{interaction.user.name}')
         em.add_field(name=new_item['Name'], value=new_item['description'])
 
-        await interaction.response.send_message(embed=em)
+        await interaction.response.send_message(embed = em, ephemeral = True)
+
+    @app_commands.command(name='delete', description='Remove an item from your inventory')
+    @app_commands.describe(item='item')
+    async def delete(self, interaction: Interaction, item: str):
+        inventory = await self.get_inventory()
+        player_inventory = inventory[str(interaction.user.id)]
+        deleted_item = item
+        for i, item in enumerate(player_inventory):
+            print(f'current index: {i}')
+            if player_inventory[i]['Name'] == deleted_item:
+                print('deleting this item')
+                del player_inventory[i]
+            else:
+                print('item not deleted')
+        print(f'New inventory {player_inventory}')
+
+        inventory[str(interaction.user.id)] = player_inventory
+        with open("inventory.json", "w") as f:
+            json.dump(inventory, f)
+            print('json saved')
+        
+        em = Embed(title = f"{interaction.user.name}'s Inventory")
+        for item in player_inventory:
+                item_name = item['Name']
+                desc = item['description']
+                em.add_field(name=item_name, value=desc)
+
+        await interaction.response.send_message(embed = em, ephemeral = True)
 
     async def get_inventory(self):
         print('get inventory called')
