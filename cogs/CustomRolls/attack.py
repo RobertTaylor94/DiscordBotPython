da04181b67b909b0c2c5ac33e0baa5b95ffa5acf
Robert Taylor <rjt903@gmail.com>
2023-06-04 12:56:08 +0100

    Feature: Added command to check users custom rolls

--- cogs/CustomRolls/attack.py
+++ cogs/CustomRolls/attack.py
@@ -10,10 +10,29 @@ class create_attack(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name = 'create_attack', description = 'Add a custom attack roll to your character')
-    @app_commands.describe(name = 'name', atbonus = 'atkbonus', dmgdice = 'dmgdice (e.g. 1d10 + 1d4)', dmgbonus = 'dmgbonus')
+    @app_commands.describe(name = 'name', atkbonus = 'atkbonus', dmgdice = 'dmgdice (e.g. 1d10 + 1d4)', dmgbonus = 'dmgbonus')
     async def create_attack(self, interaction: Interaction, name: str, atkbonus: int, dmgdice: str, dmgbonus: int, atkdice = '1d20'):
         print('running create_attack')
-    
+        custom_rolls = await self.get_custom_rolls()
+
+    @app_commands.command(name = 'check_rolls', description = 'Check your custom rolls')
+    async def check_rolls(self, interaction: Interaction):
+        print('checking user rolls...')
+        custom_rolls = await self.get_custom_rolls()
+        em = Embed(title = f"{interaction.user.name}'s Custom Rolls")
+        if str(interaction.user.id) not in custom_rolls:
+            custom_rolls[str(interaction.user.id)] = []
+            await self.save_rolls(custom_rolls)
+        else:
+            user_rolls = custom_rolls[str(interaction.user.id)]
+            for roll in user_rolls:
+                roll_name = roll['name']
+                atkbonus = roll['atkbonus']
+                dmgroll = roll['dmgroll']
+                dmgbonus = roll['dmgbonus']
+                em.add_field(name = roll_name, value = f'Attack: 1d20 + {atkbonus}\nDamage: {dmgroll} + {dmgbonus}')
+
+        await interaction.response.send_message(embed=em, ephemeral=True)
 
     async def get_custom_rolls(self):
         print('getting custom rolls...')
@@ -21,6 +40,11 @@ class create_attack(commands.Cog):
         with open('customRolls.json', 'r') as f:
             customRolls = json.load(f)
             return customRolls
+        
+    async def save_rolls(self, file):
+        with open('customRolls.json', 'w') as f:
+            json.dump(file, f)
+            print('json saved')
 
 async def setup(bot):
     await bot.add_cog(create_attack(bot))
