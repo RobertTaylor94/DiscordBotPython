67df450abb31205216c8d2b35ad628f90cafc076
Robert Taylor <rjt903@gmail.com>
2023-06-04 12:04:10 +0100

    Feature: Rewrote expression roll to allow multiple dice in expression, e.g. 1d6 + 1d4 + 5

--- cogs/expressionRoll.py
+++ cogs/expressionRoll.py
@@ -1,6 +1,7 @@
 import random
 from discord.ext import commands
 from discord import app_commands, Interaction, Embed
+import numpy as np
 
 class expressionRoll(commands.Cog):
     def __init__(self, bot):
@@ -9,40 +10,30 @@ class expressionRoll(commands.Cog):
     @app_commands.command(name='r', description='Roll a dice from an expression')
     @app_commands.describe(expression="expression")
     async def r(self, interaction: Interaction, expression: str):
-        array = expression.split("d")
-        num_of_dice = array[0]
-        num_of_sides = array[1]
+        array = expression.split("+")
+        rolls = []
+        bonus = 0
 
-        print(num_of_dice, num_of_sides)
-
-        if '+' in num_of_sides:
-            split = num_of_sides.split("+")
-            sides = int(split[0])
-            bonus = int(split[1])
-            dice = int(num_of_dice)
-            embed = roll(sides=sides, dice=dice, bonus=bonus, user=interaction.user.name)
-            await interaction.response.send_message(embed=embed)
-        else:
-            dice = int(num_of_dice)
-            sides = int(num_of_sides)
-            embed = roll(sides=sides, dice=dice, user=interaction.user.name)
-            await interaction.response.send_message(embed=embed)
-
-class diceRoll(object):
-    global roll
-    def roll(sides: int, dice: int, user, bonus = 0):
+        for i in array:
+            if "d" in i:
+                roll_array = i.split("d")
+                dice = int(roll_array[0])
+                sides = int(roll_array[1])
+                outcome = await self.roll(dice, sides)
+                rolls = np.concatenate((rolls, outcome)).astype(int)
+            else:
+                bonus += int(i)
+        total = sum(rolls)
+        total += bonus
+        em = Embed(title=interaction.user.name, description = f'{rolls} + {bonus}\n**Total: {total}**')
+        await interaction.response.send_message(embed=em)
+    
+    async def roll(self, dice: int, sides: int):
         dice_array = []
-        sum = 0
-
         for i in range(0, dice):
             rand_num = random.randint(1, sides)
             dice_array.append(rand_num)
-        for i in dice_array:
-            sum += i
-        sum += bonus
-
-        embed = Embed(type='rich', title=f'{user}', description=f'{dice_array} + {bonus}\nTotal: {sum + bonus}')
-        return embed
+        return dice_array
     
 async def setup(bot):
     await bot.add_cog(expressionRoll(bot))
