47eb5dfdb869ae65a05b5e3fff80ecbdb663cb75
Robert Taylor <rjt903@gmail.com>
2023-08-20 16:35:53 +0100

    Started add character command with choice to pick race

--- cogs/Character/characterMain.py
+++ cogs/Character/characterMain.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from discord import app_commands, Embed, Interaction
 from discord import SlashCommandGroup
+from typing import Literal
 import json
 import os
 import CharacterUtilities
@@ -11,9 +12,12 @@ class characterMain(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    utils = CharacterUtilities.characterUtils
+    util = CharacterUtilities.characterUtils
 
-    
+    @app_commands.command(name='create_character', description='Create a new character')
+    @app_commands.describe(name = 'name', race = 'race')
+    async def create_character(self, interaction: Interaction, name: str, race: Literal['dwarf', 'elf', 'gnome', 'halfling', 'human', 'orc']):
+        characters = await self.util.get_characters()
     
 
 async def setup(bot):
