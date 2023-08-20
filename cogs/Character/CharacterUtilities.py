1e45612595a9cff852b697c3797f5dc33c84d92a
Robert Taylor <rjt903@gmail.com>
2023-08-20 15:05:21 +0100

    Added intial utility functions for character management

---
+++ cogs/Character/CharacterUtilities.py
@@ -0,0 +1,40 @@
+# from discord.ext import commands
+# from discord import app_commands, Embed, Interaction
+# from discord import SlashCommandGroup
+import json
+import os 
+
+
+class characterUtils():
+    async def get_character(self):
+        os.chdir('../Character')
+        print(os.getcwd())
+        with open('characters.json', 'r') as f:
+            characters = json.load(f)
+            return characters
+    
+    async def save_characters(self, file):
+        with open('characters.json', 'w') as f:
+            json.dump(file, f)
+            print('characters.json saved')
+
+    async def get_tracks(self):
+        os.chdir('../Character')
+        print(os.getcwd())
+        with open('tracks.json', 'r') as f:
+            tracks = json.load(f)
+            return tracks
+
+    async def get_races(self):
+        os.chdir('../Character')
+        print(os.getcwd())
+        with open('races.json', 'r') as f:
+            races = json.load(f)
+            return races
+    
+    async def get_feats(self):
+        os.chdir('../Character')
+        print(os.getcwd())
+        with open('feats.json', 'r') as f:
+            feats = json.load(f)
+            return feats
