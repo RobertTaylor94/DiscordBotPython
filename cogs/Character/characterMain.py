3dbd1242481ca2a97da30a754bde2c69ba2653da
Robert Taylor <rjt903@gmail.com>
2023-08-21 10:24:26 +0100

    Init user for character creation working

--- cogs/Character/characterMain.py
+++ cogs/Character/characterMain.py
@@ -3,19 +3,25 @@ from discord import app_commands, Embed, Interaction
 from typing import Literal
 import json
 import os
-import cogs.Character.CharacterUtilities as CharacterUtilities
+from cogs.Character.CharacterUtilities import characterUtils
 
 os.chdir('cogs/Character')
 
 class characterMain(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    util = CharacterUtilities.characterUtils
+
+    util = characterUtils()
 
     @app_commands.command(name='init', description='initialise player in character database')
-    async def init_user(self, interaction: Interaction):
-        self.util.init_user(self, interaction.user)
+    async def init(self, interaction: Interaction):
+        print('init called')
+        characters = await self.util.get_characters()
+        if str(interaction.user.id) not in characters:
+            print('user not found in characters')
+            characters[str(interaction.user.id)] = []
+            print(characters)
+            await self.util.save_characters(characters)
         await interaction.response.send_message("user initialized in character database", ephemeral = True)
 
     # @app_commands.command(name='create_character', description='Create a new character')
