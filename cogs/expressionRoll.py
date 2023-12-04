3c0732f6581ed5811632c99833e8d61be9c5d8f6
Robert Taylor <rjt903@gmail.com>
2023-12-04 01:18:58 +0000

    Create and add dice rolls image to embed

--- cogs/expressionRoll.py
+++ cogs/expressionRoll.py
@@ -4,7 +4,7 @@ from discord import app_commands, Interaction, Embed, File
 import numpy as np
 from functions.roll import RollFunctions
 from functions.user_data import UserData
-from functions.get_dice_img import GetDice
+from functions.get_dice_img import stitch_dice_images
 
 class expressionRoll(commands.Cog):
     def __init__(self, bot):
@@ -14,13 +14,7 @@ class expressionRoll(commands.Cog):
     @app_commands.command(name='q', description='Roll a dice from an expression')
     @app_commands.describe(expression="expression", roll_type="rolling for")
     async def q(self, interaction: Interaction, expression: str, roll_type:str = ""):
-        array = expression.split("+")
-        # rolls = []
-        # bonus = 0
         user = UserData(interaction).get_user()
-        get_dice = GetDice()
-        files = []
-        embeds = []
 
         if roll_type != "":
             rolling = f"For {roll_type}"
@@ -32,11 +26,18 @@ class expressionRoll(commands.Cog):
         total = roll_total[2]
         bonus = roll_total[1]
         rolls = roll_total[0]
+        img_array = roll_total[3]
+
+        stitch_dice_images(img_array)
 
         em1 = Embed(title=f'{expression}\n**Total: {total}**', description=f'{rolls}')
         em1.set_author(name=f"{user.nick} {rolling}", icon_url=user.avatar_url)
-        # em1.set_image(url="https://i.imgur.com/0lU6puv.png")
-        await interaction.response.send_message(embed=em1)
+
+        file = File(f"/Users/robert/Desktop/Bot/assets/stitched_image.png", filename="image.png")
+        em1.set_image(url="attachment://image.png")
+        await interaction.response.send_message(file=file, embed=em1)
     
 async def setup(bot):
     await bot.add_cog(expressionRoll(bot))
+
+
